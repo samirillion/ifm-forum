b6c4c7741f9a9e825b0108c8a12ca5423410ded3
samirillian <sam@viridian.tech>
2017-03-22 17:43:03 -0600

    who knows

--- includes/class-controller.php
+++ includes/class-controller.php
@@ -25,11 +25,33 @@ class crowdsortController
         add_action( 'login_form_lostpassword', array( $plugin, 'redirect_to_custom_lostpassword' ) );
         add_shortcode( 'custom-password-lost-form', array( $plugin, 'render_password_lost_form' ) );
         add_action( 'login_form_lostpassword', array( $plugin, 'do_password_lost' ) );
+        add_filter( 'retrieve_password_message', array( $plugin, 'replace_retrieve_password_message' ), 10, 4 );
     }
     public function __construct()
     {
     }
+    /**
+     * Returns the message body for the password reset mail.
+     * Called through the retrieve_password_message filter.
+     *
+     * @param string  $message    Default mail message.
+     * @param string  $key        The activation key.
+     * @param string  $user_login The username for the user.
+     * @param WP_User $user_data  WP_User object.
+     *
+     * @return string   The mail message to send.
+     */
+    public function replace_retrieve_password_message( $message, $key, $user_login, $user_data ) {
+        // Create new message
+        $msg  = __( 'Hello!', 'personalize-login' ) . "\r\n\r\n";
+        $msg .= sprintf( __( 'You asked us to reset your password for your account using the email address %s.', 'personalize-login' ), $user_login ) . "\r\n\r\n";
+        $msg .= __( "If this was a mistake, or you didn't ask for a password reset, just ignore this email and nothing will happen.", 'personalize-login' ) . "\r\n\r\n";
+        $msg .= __( 'To reset your password, visit the following address:', 'personalize-login' ) . "\r\n\r\n";
+        $msg .= site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . "\r\n\r\n";
+        $msg .= __( 'Thanks!', 'personalize-login' ) . "\r\n";
 
+        return $msg;
+    }
     /**
  * Initiates password reset.
  */
