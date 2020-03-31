97af214478a3e0a078c9ff9645dc037d684a1fa9
samirillian <sam@protonmail.com>
2020-03-31 15:50:41 -0400

    working on building comments in an easier to reason about way

--- app/view/comments/comments.php
+++ app/view/comments/comments.php
@@ -1,19 +1,21 @@
 <?php
 $comment_query = $data;
-if (isset(get_post_meta($params['ifm_post_id'])['aggregator_entry_url']['0'])) {
-    $post_title_content = '<a href="' . get_post_meta($params['ifm_post_id'])['aggregator_entry_url']['0'] . '" target="_blank">' . get_the_title($params['ifm_post_id']) . '</a>';
-    $post_url           = '<a class="ifm-comment-main-url" href="' . get_post_meta($params['ifm_post_id'])['aggregator_entry_url']['0'] . '">' . get_post_meta($params['ifm_post_id'])['aggregator_entry_url']['0'] . '</a> &ndash; ';
+$ifm_post_id = $params['ifm_post_id'];
+
+if (isset(get_post_meta($ifm_post_id)['aggregator_entry_url']['0'])) {
+    $post_title_content = '<a href="' . get_post_meta($ifm_post_id)['aggregator_entry_url']['0'] . '" target="_blank">' . get_the_title($ifm_post_id) . '</a>';
+    $post_url           = '<a class="ifm-comment-main-url" href="' . get_post_meta($ifm_post_id)['aggregator_entry_url']['0'] . '">' . get_post_meta($ifm_post_id)['aggregator_entry_url']['0'] . '</a> &ndash; ';
 } else {
-    $post_title_content = get_the_title($params['ifm_post_id']);
+    $post_title_content = get_the_title($ifm_post_id);
     $post_url           = '';
 }
 echo '<div class="ifm-comment-wrapper">';
 echo '<h4 class="ifm-comment-post-title">' . $post_title_content . '</h4>';
 echo $post_url;
-echo '<span class="ifm-post-type">' . (wp_get_object_terms($params['ifm_post_id'], 'aggpost-type'))[0]->{'name'} . '</span>';
-if (get_post($params['ifm_post_id'])->post_content !== '') {
+echo '<span class="ifm-post-type">' . (wp_get_object_terms($ifm_post_id, 'aggpost-type'))[0]->{'name'} . '</span>';
+if (get_post($ifm_post_id)->post_content !== '') {
     echo '<div class="ifm-comment-main-content-wrapper">';
-    echo '<div class="ifm-comment-post-content">' . get_post($params['ifm_post_id'])->post_content . '</div>';
+    echo '<div class="ifm-comment-post-content">' . get_post($ifm_post_id)->post_content . '</div>';
     echo '</div>';
 }
 echo '<hr style="text-align:left;margin-left:0;margin-bottom:5px;width:60%;">';
--- app/view/partials/comment-form.php
+++ app/view/partials/comment-form.php
@@ -1,6 +1,7 @@
 <form action='<?php echo home_url('/api/ifm/post-comment'); ?>' method="post" id='reply-to-post'>
     <textarea class='ifm-comment' name='reply' cols='40' rows='5' required></textarea>
     <input type='submit' value='comment'>
-    <input type='hidden' name='post_id' value='<?php echo $params['ifm_post_id']; ?>'>
+    <input type='hidden' name='post_id' value='<?php xdebug_break();
+                                                echo isset($ifm_post_id) ? $ifm_post_id : get_query_var('ifm_post_id'); ?>'>
     <input type='hidden' name='nonce' value='<?php echo wp_create_nonce('wp_rest'); ?>'>
 </form>
